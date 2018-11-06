e60e1608f5e65799ab0c7daeb1473d449ab24e15
Marc <marc-gw@live.nl>
2018-11-06 16:39:09 +0100

    initial files

---
+++ Code/house.py
@@ -0,0 +1,11 @@
+
+
+class House(object):
+
+    def __init__ (self, x, y, volt):
+        self.x = x
+        self.y = y
+        self.max_volt = volt
+
+    def __str__(self):
+        return str(self.x) + str(self.y) + str(self.volt)
---
+++ Code/load.py
@@ -0,0 +1,36 @@
+from house import House
+from battery import Battery
+
+class Load():
+
+    def __init__(self, huis, batterij):
+        self.huis = self.load_houses(f"Huizen&Batterijen/{huis}_huizen.csv")
+        self.huis = self.load_batteries(f"Huizen&Batterijen/{batterij}_batterijen.txt")
+
+    def load_houses(self, filename):
+        with open(filename, "r") as f:
+            content = f.readlines()
+
+            houses = []
+
+            for i in content:
+                i = i.strip()
+                houses.append(i.split(","))
+
+            print(houses)
+            print()
+            print()
+    def load_batteries(self, filename):
+        with open(filename, "r") as f:
+            content = f.readlines()
+
+            batterys = []
+
+            for i in content:
+                i = i.strip('\n\t')
+                batterys.append(i.split("   "))
+
+            print(batterys)
+
+if __name__ == "__main__":
+    load = Load("wijk1", "wijk1")
