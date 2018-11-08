156bf3dcf6f379d255d75063a20ca27e4e7aed59
annehs <annehoogerduijn@gmail.com>
2018-11-08 15:37:02 +0100

    battery available

--- Code/battery.py
+++ Code/battery.py
@@ -1,11 +1,14 @@
 
 class Battery(object):
 
-    def __init__ (self, x, y, max_volt):
+    def __init__ (self, x, y, max_volt, current_usage):
         self.x = x
         self.y = y
         self.max_volt = max_volt
+        self.current_usage = current_usage
 
-
+    def check_volt(self):
+        available = self.max_volt - self.current_usage
+        
     def __str__(self):
         return str(self.x) + str(self.y) + str(self.max_volt)
