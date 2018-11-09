044bc4ed69c6dc4e2d5dc48bc8501ab555e49f79
annehs <annehoogerduijn@gmail.com>
2018-11-09 16:50:28 +0100

    Test & house sort

--- Code/algoritmes/load.py
+++ Code/algoritmes/load.py
@@ -23,9 +23,8 @@ class Load():
                 i = i.strip()
                 innitial_houses.append(i.split(","))
 
-            # load houses into list (x, y, max_amp)
+            # load houses into list (id, x, y, max_amp)
             for i, house in enumerate(innitial_houses[1:]):
-                print(i)
                 self.houses.append(House(i, int(house[0]), int(house[1]), float(house[2])))
 
 
@@ -45,17 +44,17 @@ class Load():
 
                 i = i.strip('\n')
                 innitial_batteries.append(i.split())
-                
+
             for i, battery in enumerate(innitial_batteries[1:]):
-                print(i)
                 self.batteries.append(Battery(i, int(battery[0]), int(battery[1]), float(battery[2])))
 
-                    # print(battery)
 
     def connect_houses(self):
 
-        print("hallo")
-        # connect & keep track of current battery usage
+        # Sort houses
+        self.houses = sorted(self.houses, key=lambda house: house.amp, reverse=True)
+
+        # connect each house to a battery (keep track of usage, max etc.)
         for house in self.houses[1:]:
             for battery in self.batteries:
                 if battery.check_amp() > house.amp:
@@ -67,6 +66,18 @@ class Load():
                     print(house)
                     break
 
+
+        # TEST
+        current_usage = 0
+
+        for battery in self.batteries:
+            print("ID:" + str(battery.id))
+            print("Current usage: " + str(battery.current_usage))
+            print("Available: " + str(battery.check_amp()))
+            print("Connected ID's" + str(battery.connected))
+            current_usage += battery.current_usage
+
+        print("Total" + str(current_usage))
 
 
 if __name__ == "__main__":
