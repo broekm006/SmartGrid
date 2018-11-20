8c624225687a6610bd60d1f16df4eb6985699ec3
Marc <marc-gw@live.nl>
2018-11-20 14:08:17 +0100

    swap fix

--- Code/algoritmes/load.py
+++ Code/algoritmes/load.py
@@ -1,5 +1,4 @@
-import random
-import sys
+import sys, random
 sys.path.append('Code/classes')
 
 from house import House
@@ -118,19 +117,20 @@ class Load():
         for house in self.houses:
             while not house.isconnected:
                 lowest, second = float('inf'), float('inf')
-
-                self.batteries = sorted(self.batteries, key=lambda battery: battery.id, reverse=True)
+                self.batteries = sorted(self.batteries, key=lambda battery: battery.id)
 
                 # get lowest & second lowest battery
                 for battery in self.batteries:
                     if battery.current_usage <= lowest:
                         lowest, second = battery.current_usage, lowest
                         battery_id1 = battery.id
-                        print("1", battery_id1, battery.current_usage)
                     elif battery.current_usage < second:
                         second = battery.current_usage
+
+                # get battery id for second
+                for battery in self.batteries:
+                    if second == battery.current_usage:
                         battery_id2 = battery.id
-                        print("2", battery_id2, battery.current_usage)
 
                 # choose random id from battery-house list
                 random1 = random.choice(self.batteries[battery_id1].connected)
@@ -140,11 +140,9 @@ class Load():
                 for house in self.houses:
                     if house.id == random1:
                         random1_amp = house.amp
-                        #print(house.amp)
 
                     if house.id == random2:
                         random2_amp = house.amp
-                        #print(house.amp)
 
                 #remove "lowest id + update current_usage"
                 self.batteries[battery_id1].remove(random1)
@@ -160,24 +158,24 @@ class Load():
                 if  max - (currents1 + random2_amp) > 0 and max - (currents2 + random1_amp) > 0:
                     #add removed house to other battery_id
                     self.batteries[battery_id1].connect(random2)
-                    #self.batteries[battery_id1].add(random2_amp)
-                    self.batteries[battery_id1].current_usage += random2_amp
+                    self.batteries[battery_id1].add(random2_amp)
+                    #self.batteries[battery_id1].current_usage += random2_amp
 
 
                     self.batteries[battery_id2].connect(random1)
-                    #self.batteries[battery_id2].add(random1_amp)
-                    self.batteries[battery_id2].current_usage += random1_amp
+                    self.batteries[battery_id2].add(random1_amp)
+                    #self.batteries[battery_id2].current_usage += random1_amp
 
                 else:
                     #undo remove because the switch does not work (battery overload)
                     self.batteries[battery_id1].connect(random1)
-                    #self.batteries[battery_id1].add(random1_amp)
-                    self.batteries[battery_id1].current_usage += random1_amp
+                    self.batteries[battery_id1].add(random1_amp)
+                    #self.batteries[battery_id1].current_usage += random1_amp
 
 
                     self.batteries[battery_id2].connect(random2)
-                    #self.batteries[battery_id2].add(random2_amp)
-                    self.batteries[battery_id2].current_usage += random2_amp
+                    self.batteries[battery_id2].add(random2_amp)
+                    #self.batteries[battery_id2].current_usage += random2_amp
 
                 #calculate new amp
                 low_max = self.batteries[battery_id1].current_usage
@@ -187,49 +185,34 @@ class Load():
                 if  low_max <= self.batteries[battery_id1].max_amp - house.amp:
                     self.batteries[battery_id1].connect(house.id)
                     self.batteries[battery_id1].add(house.amp)
-                    #print("LOW", low_max - house.amp)
                     house.isconnected = True
 
                 # check if left_over house fits in second lowest value battery
                 elif high_max <= self.batteries[battery_id2].max_amp - house.amp:
                     self.batteries[battery_id2].connect(house.id)
                     self.batteries[battery_id2].add(house.amp)
-                    #print("high", high_max - house.amp)
                     house.isconnected = True
 
                 counter += 1
-                '''
-                #print statements to test
-                print("house1 amp:", random1_amp)
-                print("house2 amp:", random2_amp)
-                print(house.amp)
-                print("battery1 cap:", random2_amp + lowest - random1_amp)
-                print("battery2 cap:", random1_amp + second - random2_amp)
-                print("room left battery1:", low_max, ">id<", battery_id1)
-                print("room left battery2:", self.batteries[battery_id2].max_amp - (random1_amp + second - random2_amp), ">id<", battery_id2)
-                '''
-                #!# voeg check house . costs error > zou goed moeten zijn nadat functie is gerunt
-                #!# voeg check to op basis van max_amp per battery zodat deze niet kan worden overschreven.
-
-
+        print(counter)
         # TEST: alle batterijen aangesloten?
         # Batterijen totaal = 7 535
         # Huizen totaal = 7 500
+        '''
+        print()
+        print(counter)
+        current_usage = 0
+        for battery in self.batteries:
+            print()
+            print("ID:" + str(battery.id))
+            print("Current usage: " + str(battery.current_usage))
+            print("Available: " + str(battery.check_amp()))
+            print("Connected ID's" + str(battery.connected))
+            current_usage += battery.current_usage
+            print()
 
-                print()
-                print(counter)
-                current_usage = 0
-                for battery in self.batteries:
-                    print()
-                    print("ID:" + str(battery.id))
-                    print("Current usage: " + str(battery.current_usage))
-                    print("Available: " + str(battery.check_amp()))
-                    print("Connected ID's" + str(battery.connected))
-                    current_usage += battery.current_usage
-                    print()
-
-                print("Total battery usage: " + str(current_usage))
-
+        print("Total battery usage: " + str(current_usage))
+        '''
 
         '''
         # TEST: overzicht huizen gesorteert op basis van kosten
