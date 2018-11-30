65d2120339e638fa8f4eaf908de42c492642069b
tjmf94 <thomasfranx@hotmail.com>
2018-11-30 15:19:59 +0100

    LaTeST UpDaTeZ 4 K-MeanZ GrTjS

--- Code/visualizer.py
+++ Code/visualizer.py
@@ -74,7 +74,7 @@ def create_visualisation(houses_csv, batteries_csv):
     sns.set_color_codes("dark")
     #sns.palplot(sns.color_palette())
     plot = sns.scatterplot(x="x", y="y", hue="connected_bat", data=df, ci=None, style="type", palette=["C0", "C1", "C2", "C3", "C4"])
-    plot.set_title("Wijk 1: Houses sorted by output before Hill Climber")
+    plot.set_title("Wijk 1: houses connected to nearest battery without restrictions.")
     plot.legend_.remove()
     plot.set(xlabel='X coordinates', ylabel='Y coordinates')
 
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from visualizer import Visualizer
 sys.path.append('Code/algoritmes')
 from greedy import Greedy
 from hill_climber import Hill_climber
+from k_means import K_means
 
 sys.path.append('Code/classes')
 from helper import Helper
@@ -16,14 +17,16 @@ from helper import Helper
 if __name__ == "__main__":
     load = Load("wijk1", "wijk1")
 
-    Helper.bounds(Helper, load.batteries, load.houses)
-    greedy = Greedy(load.houses, load.batteries, "output") # "output","distance", "priority"
+    k = K_means(load.houses, load.batteries)
 
-
-    hill_climber = Hill_climber(load.houses, load.batteries, 500)
-    hill_climber.ice_climbers()
-
-    Helper.costs(Helper, hill_climber.batteries, hill_climber.houses)
-    Helper.sort_houses(Helper, greedy.houses)
-
-    Visualizer(hill_climber.houses, hill_climber.batteries)
+    # Helper.bounds(Helper, load.batteries, load.houses)
+    # greedy = Greedy(load.houses, load.batteries, "output") # "output","distance", "priority"
+    #
+    #
+    # hill_climber = Hill_climber(load.houses, load.batteries, 500)
+    # hill_climber.ice_climbers()
+    #
+    # Helper.costs(Helper, hill_climber.batteries, hill_climber.houses)
+    # Helper.sort_houses(Helper, greedy.houses)
+    #
+    # Visualizer(hill_climber.houses, hill_climber.batteries)
