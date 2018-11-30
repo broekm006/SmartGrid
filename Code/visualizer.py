4bd87daf5697a8e91cd294877bdbaa8d3a87bcce
annehs <annehoogerduijn@gmail.com>
2018-11-30 16:14:40 +0100

    k-means 2 + deel van best choice

--- Code/visualizer.py
+++ Code/visualizer.py
@@ -74,7 +74,7 @@ def create_visualisation(houses_csv, batteries_csv):
     sns.set_color_codes("dark")
     #sns.palplot(sns.color_palette())
     plot = sns.scatterplot(x="x", y="y", hue="connected_bat", data=df, ci=None, style="type", palette=["C0", "C1", "C2", "C3", "C4"])
-    plot.set_title("Wijk 1: Houses sorted by output after Hill Climber")
+    plot.set_title("Wijk 1: after K-Means")
     plot.legend_.remove()
     plot.set(xlabel='X coordinates', ylabel='Y coordinates')
 
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ sys.path.append('Code')
 from load import Load
 from visualizer import Visualizer
 
-
 sys.path.append('Code/algoritmes')
 from greedy import Greedy
 from hill_climber import Hill_climber
@@ -16,18 +15,18 @@ from helper import Helper
 
 
 if __name__ == "__main__":
-    load = Load("wijk1", "wijk1")
+    load = Load("wijk3", "wijk3")
 
     Helper.bounds(Helper, load.batteries, load.houses)
-    # greedy = Greedy(load.houses, load.batteries, "priority") # "output", "distance", "priority"
+    # greedy = Greedy(load.houses, load.batteries, "output") # "output", "distance", "priority"
 
     k_means = K_means(load.houses, load.batteries)
-    # hill_climber = Hill_climber_test(load.houses, load.batteries, 500)
+    # hill_climber = Hill_climber_test(k_means.houses, k_means.batteries, 500)
     # hill_climber.best_choice()
     # hill_climber.ice_climbers()
     # #
-    # # Helper.costs(Helper, hill_climber.batteries, hill_climber.houses)
-    # # Helper.sort_houses(Helper, greedy.houses)
+    # Helper.costs(Helper, k_means.batteries, k_means.houses)
+    # Helper.sort_houses(Helper, k_means.houses)
     # #
     # Visualizer(hill_climber.houses, hill_climber.batteries)
     Visualizer(k_means.houses, k_means.batteries)
