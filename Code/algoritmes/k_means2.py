c605076eaae34174f0e09c521b06fef74fcf0aa5
annehs <annehoogerduijn@gmail.com>
2018-12-18 00:18:05 +0100

    removed comments

--- Code/algoritmes/k_means2.py
+++ Code/algoritmes/k_means2.py
@@ -24,64 +24,10 @@ class K_means2(object):
             battery.x = random.randint(0, 50)
             battery.y = random.randint(0, 50)
 
-    # def clustering(self, houses, batteries, counter):
-    #     ''' Use K-means to cluster and assign houses to batteries '''
-    #     # count interations
-    #     counter += 1
-    #
-    #     while(True):
-    #         # capacitated clustering
-    #         greedy = Greedy(houses, batteries, self.greedy)
-    #         houses = copy.deepcopy(greedy.houses)
-    #         batteries = copy.deepcopy(greedy.batteries)
-    #
-    #         # for battery in batteries:
-    #         #     houses = Sort.distance(Sort, houses, battery)
-    #
-    #         # calculate  cable costs
-    #         costs = Helper.costs(Helper, batteries, houses)
-    #         print(costs)
-    #
-    #         # for each cluster, new centre = means of all points x
-    #         for battery in batteries:
-    #             x = 0
-    #             y = 0
-    #             counter = 0
-    #
-    #             for house in battery.connected:
-    #                 x += house.x
-    #                 y += house.y
-    #                 counter += 1
-    #
-    #             # average
-    #             mean_x = round(x / counter)
-    #             mean_y = round(y / counter)
-    #
-    #             # new center
-    #             battery.x = mean_x
-    #             battery.y = mean_y
-    #
-    #         # Stops when costs haven't changed
-    #         if costs < self.costs:
-    #             self.costs = costs
-    #             break
-    #
-    #         # disconnect
-    #         for battery in batteries:
-    #             battery.connected = []
-    #             battery.current_usage = 0
-    #
-    #         for house in houses:
-    #             house.connected = False
-    #             house.connection = False
-    #             house.costs = 0
-    #
-    #         # --> Solution
-    #         self.batteries = copy.deepcopy(batteries)
-    #         self.houses = copy.deepcopy(houses)
-
     def clustering(self, houses, batteries, counter):
-        # count interations
+        ''' Use K-means to cluster and assign houses to batteries '''
+
+        # count clustering iterations
         counter += 1
 
         # capacitated clustering
@@ -92,15 +38,14 @@ class K_means2(object):
         for battery in batteries:
             houses = Sort.distance(Sort, houses, battery)
 
-        # calculate  cable costs
+        # calculate cable costs
         cable_costs = Helper.houses_costs(Helper, batteries, houses)
         battery_costs = 0
         for battery in batteries:
             battery_costs += battery.cost
         costs = cable_costs + battery_costs
-        # print(costs)
 
-        # for each cluster, new centre = means of all points x
+        # for each cluster, new centre = means of all connected houses coördinates
         for battery in batteries:
             x = 0
             y = 0
@@ -111,6 +56,7 @@ class K_means2(object):
                 y += house.y
                 count += 1
 
+            # Avoid dividing by zero battery is not connected to any house (HAC)
             if count != 0:
                 # average
                 mean_x = round(x / count)
@@ -141,9 +87,6 @@ class K_means2(object):
             # try again
             self.clustering(houses, batteries, counter)
         else:
-            # Save solution & append to Greedy-solution(list)
-
-            # solution.km_solution()
             pass
 
     def results(self):
@@ -155,7 +98,6 @@ class K_means2(object):
         costs = k_means_solution.calculate_costs2()
         k_means_solution.bounds(self.batteries, self.houses)
 
-        # costs = k_means_solution.calculate_costs2()
         print()
         print("TOTAL COSTS AFTER K-MEANS: ", costs)
 
